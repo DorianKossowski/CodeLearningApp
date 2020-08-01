fe62d3b976bf69e1e3530ba3ae174f9fbf2ff303
DorianKossowski <doriancfc@gmail.com>
2020-08-01 23:42:28 +0200

    Create test endpoint for parsing

---
+++ back/src/main/java/com/server/app/controller/TestParseController.java
@@ -0,0 +1,15 @@
+package com.server.app.controller;
+
+import com.server.app.model.dto.TestInputDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestParseController {
+
+    @PostMapping("/parse")
+    public String parseInput(@RequestBody TestInputDto input) {
+        return "Parsing: " + input.getInput();
+    }
+}
---
+++ back/src/main/java/com/server/app/model/dto/TestInputDto.java
@@ -0,0 +1,13 @@
+package com.server.app.model.dto;
+
+public class TestInputDto {
+    private String input;
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+}
