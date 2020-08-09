9791e53be0aa0733ba4d5a0a33981ba22eeac6fc
DorianKossowski <doriancfc@gmail.com>
2020-08-09 19:12:18 +0200

    Add POC Home page

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,12 +1,21 @@
+import './components.css';
+
 import React, { Component } from 'react';
+
+import { Container } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 class Home extends Component {
 
     render = () => {
         return (
-            <>
-            <h2>Kurs programowania JAVA</h2>
-            </>
+            <Container className='home'>
+                <h2>Kurs programowania JAVA</h2>
+                <div className='category'>
+                    <p className='categoryTitle'>Wprowadzenie</p><div className='categoryTitleUnderline'/>
+                    <div className='categoryElement'><LinkContainer to={ 'hello' }><a className='categoryElementText'>1. Pierwszy program - Hello World</a></LinkContainer></div><div className='categoryElementUnderline'/>
+                </div>
+            </Container>
         );
     }
 }
