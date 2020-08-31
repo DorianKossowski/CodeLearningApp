e93e6f87a5163a3327387167a2d0a96be5388b6f
DorianKossowski <doriancfc@gmail.com>
2020-08-31 22:44:48 +0200

    Add missing requireNonNull

--- back/src/main/java/com/server/parser/java/ast/Expression.java
+++ back/src/main/java/com/server/parser/java/ast/Expression.java
@@ -1,10 +1,12 @@
 package com.server.parser.java.ast;
 
+import java.util.Objects;
+
 public class Expression extends AstElement {
     private final String text;
 
     public Expression(String text) {
-        this.text = text;
+        this.text = Objects.requireNonNull(text, "text cannot be null");
     }
 
     public String getText() {
--- back/src/main/java/com/server/parser/java/ast/MethodCall.java
+++ back/src/main/java/com/server/parser/java/ast/MethodCall.java
@@ -1,14 +1,15 @@
 package com.server.parser.java.ast;
 
 import java.util.List;
+import java.util.Objects;
 
 public class MethodCall extends Statement {
     private final String name;
     private final List<Expression> args;
 
     public MethodCall(String name, List<Expression> args) {
-        this.name = name;
-        this.args = args;
+        this.name = Objects.requireNonNull(name, "name cannot be null");
+        this.args = Objects.requireNonNull(args, "args cannot be null");
     }
 
     public String getName() {
