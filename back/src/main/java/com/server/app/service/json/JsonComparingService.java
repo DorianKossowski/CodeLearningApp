80b90219c0ecc558d80ae071bc3e8d34e38d98b3
DorianKossowski <doriancfc@gmail.com>
2020-09-18 20:34:27 +0200

    Remove json classes

--- back/src/main/java/com/server/app/service/json/JsonComparingService.java
+++
@@ -1,6 +0,0 @@
-package com.server.app.service.json;
-
-public interface JsonComparingService {
-
-    boolean areEqual(String json1, String json2);
-}
--- back/src/main/java/com/server/app/service/json/TaskToJsonService.java
+++
@@ -1,7 +0,0 @@
-package com.server.app.service.json;
-
-import com.server.parser.java.ast.TaskAst;
-
-public interface TaskToJsonService {
-    String mapToJson(TaskAst taskAst);
-}
