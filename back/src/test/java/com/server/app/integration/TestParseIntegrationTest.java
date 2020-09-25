fad17b637b54f242246a14f14a63a4cf69931a79
DorianKossowski <doriancfc@gmail.com>
2020-09-25 23:29:16 +0200

    Add integration test for not satisfy input

--- back/src/test/java/com/server/app/integration/TestParseIntegrationTest.java
+++ back/src/test/java/com/server/app/integration/TestParseIntegrationTest.java
@@ -17,10 +17,28 @@ class TestParseIntegrationTest extends IntegrationTestBase {
 
     @Test
     void shouldReturnValidVerificationResult() throws Exception {
-        String testCase = ParserAcceptanceTestCasesProvider.getSingleTestCase("app/integration/valid-parse-input");
-        AcceptanceTestCaseModel acceptanceTestCaseModel = AcceptanceTestCaseFetcher.fetchModel(testCase);
-        TestInputDto inputDto = new TestInputDto(acceptanceTestCaseModel.getTask(), acceptanceTestCaseModel.getInput());
+        TestInputDto inputDto = createTestInputDto("app/integration/valid-parse-input");
         VerificationResultDto resultDto = VerificationResultDto.valid();
+
+        mvc.perform(post("/parse")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(inputDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.errorMessage").value(resultDto.getErrorMessage()))
+                .andExpect(jsonPath("$.lineNumber").value(resultDto.getLineNumber()));
+    }
+
+    private TestInputDto createTestInputDto(String casePath) {
+        String testCase = ParserAcceptanceTestCasesProvider.getSingleTestCase(casePath);
+        AcceptanceTestCaseModel acceptanceTestCaseModel = AcceptanceTestCaseFetcher.fetchModel(testCase);
+        return new TestInputDto(acceptanceTestCaseModel.getTask(), acceptanceTestCaseModel.getInput());
+    }
+
+    @Test
+    void shouldReturnInvalidVerificationResultWhenNotSatisfy() throws Exception {
+        TestInputDto inputDto = createTestInputDto("app/integration/not-satisfy-parse-input");
+        VerificationResultDto resultDto = VerificationResultDto.invalid(
+                "Oczekiwana instrukcja \"Wywołanie metody z literału\" nie istnieje");
 
         mvc.perform(post("/parse")
                 .contentType(MediaType.APPLICATION_JSON)
