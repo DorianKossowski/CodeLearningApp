18d9d81df691ce05b39478e4ad6a05e104307e27
DorianKossowski <doriancfc@gmail.com>
2020-09-25 20:12:46 +0200

    Create base for integration tests

---
+++ back/src/test/java/com/server/app/integration/IntegrationTestBase.java
@@ -0,0 +1,22 @@
+package com.server.app.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class IntegrationTestBase {
+    @Autowired
+    protected MockMvc mvc;
+
+    protected static String asJsonString(Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
