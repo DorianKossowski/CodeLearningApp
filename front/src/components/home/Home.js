914da6719d01989d40e4dfea8a57328c309be46d
DorianKossowski <doriancfc@gmail.com>
2020-09-27 23:06:29 +0200

    Support collapse in home

--- front/src/components/home/Home.js
+++ front/src/components/home/Home.js
@@ -1,9 +1,9 @@
-import './home.css';
-
 import React, { Component } from 'react';
 
 import { Container } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
+import HomeIntroduction from './HomeIntroduction';
+
+import './home.css';
 
 class Home extends Component {
 
@@ -11,12 +11,7 @@ class Home extends Component {
         return (
             <Container className='home'>
                 <h2>Kurs programowania JAVA</h2>
-                <div className='category'>
-                    <p className='categoryTitle'>Wprowadzenie</p><div className='categoryTitleUnderline'/>
-                    <div className='categoryElement'>
-                        <LinkContainer to={'task/introduction/1'}><a className='categoryElementText'>1. Pierwszy program - Hello World</a></LinkContainer>
-                    </div><div className='categoryElementUnderline'/>
-                </div>
+                <HomeIntroduction />
             </Container>
         );
     }
---
+++ front/src/components/home/HomeIntroduction.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import {Collapse} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
+import {FaCaretDown, FaCaretUp} from 'react-icons/fa';
+
+const HomeIntroduction = (props) => {
+    const [open, setOpen] = useState(true);
+
+    const DynamiCaretTag = open ? FaCaretUp : FaCaretDown;
+    return (
+    <div className='category'>
+        <div className='categoryTitleArea'>
+            <p className='categoryTitle'>Wprowadzenie</p>
+            <DynamiCaretTag className='titleCaret' onClick={() => setOpen(!open)} aria-controls='elements' aria-expanded={open} />
+        </div>
+        <div className='categoryTitleUnderline'/>
+        <Collapse in={open}>
+            <div id='elements'>
+                <div className='categoryElement' >
+                    <LinkContainer to={'task/introduction/1'}><a className='categoryElementText'>1. Pierwszy program - Hello World</a></LinkContainer>
+                    <div className='categoryElementUnderline'/>
+                </div>
+                <div className='categoryElement' >
+                    <LinkContainer to={'task/introduction/2'}><a className='categoryElementText'>2. Pierwszy program - My Hello World</a></LinkContainer>
+                    <div className='categoryElementUnderline'/>
+                </div>
+                <div className='categoryElement' >
+                    <LinkContainer to={'task/introduction/3'}><a className='categoryElementText'>3. Pierwszy program - Hello World z użyciem zmiennej</a></LinkContainer>
+                    <div className='categoryElementUnderline'/>
+                </div>
+            </div>
+        </Collapse>
+    </div>
+    );
+}
+
+export default HomeIntroduction;
