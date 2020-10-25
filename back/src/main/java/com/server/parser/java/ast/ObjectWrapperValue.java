f4a709bebe688af60a7008b4b93e6ab1dfd48f07
DorianKossowski <doriancfc@gmail.com>
2020-10-25 20:44:35 +0100

    Support equality checking for wrapper objects

--- back/src/main/java/com/server/parser/java/ast/ObjectWrapperValue.java
+++ back/src/main/java/com/server/parser/java/ast/ObjectWrapperValue.java
@@ -3,6 +3,7 @@ package com.server.parser.java.ast;
 import com.server.parser.java.ast.constant.BooleanConstant;
 import com.server.parser.java.ast.constant.Constant;
 import com.server.parser.java.ast.expression.Literal;
+import com.server.parser.util.EqualityService;
 
 public class ObjectWrapperValue extends ObjectValue implements ConstantProvider {
     protected final Constant<?> constant;
@@ -24,6 +25,14 @@ public class ObjectWrapperValue extends ObjectValue implements ConstantProvider 
 
     @Override
     public BooleanConstant equalsV(Value v2) {
+        if (v2 instanceof PrimitiveValue) {
+            PrimitiveValue primitiveValue = (PrimitiveValue) v2;
+            return constant.equalsC(primitiveValue.getConstant(), EqualityService.EqualityType.PRIMITIVE);
+        }
+        if (v2 instanceof ObjectWrapperValue) {
+            ObjectWrapperValue wrapperValue = (ObjectWrapperValue) v2;
+            return constant.equalsC(wrapperValue.getConstant(), EqualityService.EqualityType.OBJECT);
+        }
         throw new UnsupportedOperationException();
     }
 }
