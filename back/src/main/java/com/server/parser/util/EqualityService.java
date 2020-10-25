ee479b8d8cfe989a79f4b222fdf4d382bb3fa1a0
DorianKossowski <doriancfc@gmail.com>
2020-10-25 21:56:39 +0100

    Add missing types for EqualityService

--- back/src/main/java/com/server/parser/util/EqualityService.java
+++ back/src/main/java/com/server/parser/util/EqualityService.java
@@ -28,21 +28,21 @@ public class EqualityService {
     }
 
     public static BooleanConstant check(CharacterConstant constant1, Constant<?> constant2, EqualityType type) {
-        if (constant2 instanceof CharacterConstant || constant2 instanceof IntConstant) {
+        if (constant2 instanceof CharacterConstant || constant2 instanceof IntConstant || constant2 instanceof DoubleConstant) {
             return new BooleanConstant(typeToOperation.get(type).apply(constant1.c, constant2.c));
         }
         throw new ResolvingException("Nie można porównać typu znakowego z " + constant2.c.getClass().getSimpleName());
     }
 
     public static BooleanConstant check(DoubleConstant constant1, Constant<?> constant2, EqualityType type) {
-        if (constant2 instanceof DoubleConstant) {
+        if (constant2 instanceof DoubleConstant || constant2 instanceof IntConstant || constant2 instanceof CharacterConstant) {
             return new BooleanConstant(typeToOperation.get(type).apply(constant1.c, constant2.c));
         }
         throw new ResolvingException("Nie można porównać typu dziesiętnego z " + constant2.c.getClass().getSimpleName());
     }
 
     public static BooleanConstant check(IntConstant constant1, Constant<?> constant2, EqualityType type) {
-        if (constant2 instanceof IntConstant || constant2 instanceof CharacterConstant) {
+        if (constant2 instanceof IntConstant || constant2 instanceof DoubleConstant || constant2 instanceof CharacterConstant) {
             return new BooleanConstant(typeToOperation.get(type).apply(constant1.c, constant2.c));
         }
         throw new ResolvingException("Nie można porównać typu całkowitego z " + constant2.c.getClass().getSimpleName());
