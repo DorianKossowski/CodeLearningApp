0d2827bdadca4d3e1ceae68b8b7bac9124a3a10a
DorianKossowski <doriancfc@gmail.com>
2021-02-13 15:10:29 +0100

    Use invocation parameters

--- back/src/main/java/com/server/parser/java/call/CallExecutor.java
+++ back/src/main/java/com/server/parser/java/call/CallExecutor.java
@@ -1,9 +1,12 @@
 package com.server.parser.java.call;
 
+import com.google.common.collect.Streams;
 import com.server.parser.java.ast.Method;
+import com.server.parser.java.ast.expression.Expression;
 import com.server.parser.java.ast.statement.CallInvocation;
 import com.server.parser.java.ast.statement.CallStatement;
 import com.server.parser.java.ast.statement.Statement;
+import com.server.parser.java.ast.statement.VariableDef;
 import com.server.parser.java.context.ContextCopyFactory;
 import com.server.parser.java.context.JavaContext;
 import com.server.parser.java.visitor.StatementListVisitor;
@@ -13,6 +16,7 @@ import java.io.Serializable;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class CallExecutor implements Serializable {
     static final int MAX_EXECUTION_LEVEL = 10;
@@ -30,9 +34,17 @@ public class CallExecutor implements Serializable {
     public CallStatement execute(Method method, CallInvocation invocation) {
         prepareForNextExecution();
         JavaContext executionContext = ContextCopyFactory.createExecutionContext(method.getMethodContext());
+        assignInvocationParameters(method.getHeader().getArguments(), invocation.getArgs(), executionContext);
         List<Statement> statements = visitor.visit(method.getBodyContext(), executionContext);
         --executionLevel;
         return new CallStatement(invocation, statements);
+    }
+
+    void assignInvocationParameters(List<VariableDef> arguments, List<Expression> invocationParameters, JavaContext executionContext) {
+        List<String> argumentsNames = arguments.stream()
+                .map(VariableDef::getName)
+                .collect(Collectors.toList());
+        Streams.forEachPair(argumentsNames.stream(), invocationParameters.stream(), executionContext::updateVariable);
     }
 
     void prepareForNextExecution() {
--- back/src/test/java/com/server/parser/java/call/CallExecutorTest.java
+++ back/src/test/java/com/server/parser/java/call/CallExecutorTest.java
@@ -5,9 +5,8 @@ import com.server.parser.java.JavaParser;
 import com.server.parser.java.ast.Method;
 import com.server.parser.java.ast.expression.Expression;
 import com.server.parser.java.ast.statement.CallInvocation;
-import com.server.parser.java.ast.statement.CallStatement;
-import com.server.parser.java.ast.statement.ExpressionStatement;
-import com.server.parser.java.ast.statement.Statement;
+import com.server.parser.java.ast.statement.*;
+import com.server.parser.java.context.JavaContext;
 import com.server.parser.java.visitor.StatementListVisitor;
 import com.server.parser.util.exception.ResolvingException;
 import org.junit.jupiter.api.BeforeEach;
@@ -28,6 +27,8 @@ import static org.mockito.Mockito.*;
 class CallExecutorTest {
     @Mock
     private StatementListVisitor visitor;
+    @Mock
+    private JavaContext context;
 
     private CallExecutor executor;
 
@@ -67,6 +68,7 @@ class CallExecutorTest {
         Method method = mock(Method.class, RETURNS_DEEP_STUBS);
         JavaParser.MethodBodyContext bodyContext = mock(JavaParser.MethodBodyContext.class);
         when(method.getBodyContext()).thenReturn(bodyContext);
+        when(method.getHeader().getArguments()).thenReturn(Collections.emptyList());
         ExpressionStatement expressionStatement = mock(ExpressionStatement.class);
         doCallRealMethod().when(expressionStatement).getExpressionStatements();
         when(visitor.visit(eq(bodyContext), any())).thenReturn(Collections.singletonList(expressionStatement));
@@ -87,5 +89,19 @@ class CallExecutorTest {
         assertThatThrownBy(() -> executor.prepareForNextExecution())
                 .isExactlyInstanceOf(ResolvingException.class)
                 .hasMessage("Problem podczas rozwiązywania: Przekroczono ilość dopuszczalnych zagnieżdżonych wywołań równą: 10");
+    }
+
+    @Test
+    void shouldAssignParameters() {
+        String name = "name";
+        VariableDef variableDef = mock(VariableDef.class);
+        when(variableDef.getName()).thenReturn(name);
+        List<VariableDef> arguments = Collections.singletonList(variableDef);
+        Expression expression = mock(Expression.class);
+        List<Expression> invocationParameters = Collections.singletonList(expression);
+
+        executor.assignInvocationParameters(arguments, invocationParameters, context);
+
+        verify(context).updateVariable(name, expression);
     }
 }
