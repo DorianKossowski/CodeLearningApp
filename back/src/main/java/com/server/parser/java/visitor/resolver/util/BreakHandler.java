d827c7f33d457b603f23acb000c20f44c6629b2e
DorianKossowski <doriancfc@gmail.com>
2021-02-17 20:05:40 +0100

    Create BreakHandler

---
+++ back/src/main/java/com/server/parser/java/visitor/resolver/util/BreakHandler.java
@@ -0,0 +1,30 @@
+package com.server.parser.java.visitor.resolver.util;
+
+import com.google.common.collect.ImmutableSet;
+import com.server.parser.java.ast.statement.*;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class BreakHandler {
+    private static final Set<Class<? extends Statement>> breakableStatementClasses =
+            ImmutableSet.of(SwitchStatement.class, ForStatement.class, WhileStatement.class, DoWhileStatement.class);
+
+    public static boolean shouldBreak(Statement currentStatement) {
+        return getStatementsToFind(currentStatement).stream()
+                .flatMap(innerStatement -> innerStatement.getExpressionStatements().stream())
+                .anyMatch(expressionStatement -> expressionStatement instanceof BreakStatement);
+    }
+
+    private static List<Statement> getStatementsToFind(Statement statement) {
+        List<Statement> statements = statement instanceof BlockStatement ?
+                ((BlockStatement) statement).getStatements() :
+                Collections.singletonList(statement);
+
+        return statements.stream()
+                .filter(innerStatement -> !breakableStatementClasses.contains(innerStatement.getClass()))
+                .collect(Collectors.toList());
+    }
+}
---
+++ back/src/test/java/com/server/parser/java/visitor/resolver/util/BreakHandlerTest.java
@@ -0,0 +1,36 @@
+package com.server.parser.java.visitor.resolver.util;
+
+import com.server.parser.java.ast.statement.BlockStatement;
+import com.server.parser.java.ast.statement.BreakStatement;
+import com.server.parser.java.ast.statement.ForStatement;
+import com.server.parser.java.ast.statement.IfElseStatement;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BreakHandlerTest {
+
+    @Test
+    void shouldFindBreak() {
+        IfElseStatement ifElseStatement = IfElseStatement.createIf("", BreakStatement.INSTANCE);
+
+        assertThat(BreakHandler.shouldBreak(ifElseStatement)).isTrue();
+    }
+
+    @Test
+    void shouldFindBreakInBlock() {
+        IfElseStatement ifElseStatement = IfElseStatement.createIf("", BreakStatement.INSTANCE);
+        BlockStatement blockStatement = new BlockStatement(Collections.singletonList(ifElseStatement));
+
+        assertThat(BreakHandler.shouldBreak(blockStatement)).isTrue();
+    }
+
+    @Test
+    void shouldNotFindBreak() {
+        ForStatement ifElseStatement = new ForStatement(Collections.singletonList(BreakStatement.INSTANCE));
+
+        assertThat(BreakHandler.shouldBreak(ifElseStatement)).isFalse();
+    }
+}
