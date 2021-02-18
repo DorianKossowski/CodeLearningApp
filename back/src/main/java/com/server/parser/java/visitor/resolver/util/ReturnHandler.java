e07ea9bb2d61a3866a3d53bcaf93f1662b9d451e
DorianKossowski <doriancfc@gmail.com>
2021-02-18 22:42:29 +0100

    Handle return in CallStatement

--- back/src/main/java/com/server/parser/java/visitor/resolver/util/ReturnHandler.java
+++ back/src/main/java/com/server/parser/java/visitor/resolver/util/ReturnHandler.java
@@ -1,11 +1,15 @@
 package com.server.parser.java.visitor.resolver.util;
 
+import com.server.parser.java.ast.statement.CallStatement;
 import com.server.parser.java.ast.statement.ReturnStatement;
 import com.server.parser.java.ast.statement.Statement;
 
 public class ReturnHandler {
 
     public static boolean shouldReturn(Statement currentStatement) {
+        if (currentStatement instanceof CallStatement) {
+            return false;
+        }
         return currentStatement.getExpressionStatements().stream()
                 .anyMatch(expressionStatement -> expressionStatement instanceof ReturnStatement);
     }
--- back/src/test/java/com/server/parser/java/visitor/resolver/util/ReturnHandlerTest.java
+++ back/src/test/java/com/server/parser/java/visitor/resolver/util/ReturnHandlerTest.java
@@ -1,9 +1,13 @@
 package com.server.parser.java.visitor.resolver.util;
 
 import com.server.parser.java.ast.expression.Expression;
+import com.server.parser.java.ast.statement.CallInvocation;
+import com.server.parser.java.ast.statement.CallStatement;
 import com.server.parser.java.ast.statement.IfElseStatement;
 import com.server.parser.java.ast.statement.ReturnStatement;
 import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -15,5 +19,13 @@ class ReturnHandlerTest {
         IfElseStatement ifElseStatement = IfElseStatement.createIf("", new ReturnStatement("", mock(Expression.class)));
 
         assertThat(ReturnHandler.shouldReturn(ifElseStatement)).isTrue();
+    }
+
+    @Test
+    void shouldNotReturnWhenCallStatement() {
+        ReturnStatement returnStatement = new ReturnStatement("", mock(Expression.class));
+        CallStatement callStatement = new CallStatement(mock(CallInvocation.class), Collections.singletonList(returnStatement));
+
+        assertThat(ReturnHandler.shouldReturn(callStatement)).isFalse();
     }
 }
