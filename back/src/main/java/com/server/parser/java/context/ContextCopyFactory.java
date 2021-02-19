ff6974c16ecaf47b2c6be56d730d127b6ad6f9e8
DorianKossowski <doriancfc@gmail.com>
2021-02-19 18:31:17 +0100

    Create FieldVarInitExpressionSupplier

--- back/src/main/java/com/server/parser/java/context/ContextCopyFactory.java
+++ back/src/main/java/com/server/parser/java/context/ContextCopyFactory.java
@@ -1,15 +1,22 @@
 package com.server.parser.java.context;
 
 import com.rits.cloning.Cloner;
+import com.server.parser.java.ast.FieldVarInitExpressionSupplier;
 
 public class ContextCopyFactory {
 
     public static JavaContext createValidationContext(JavaContext baseContext) {
-        return new Cloner().deepClone(baseContext);
+        return createCloner().deepClone(baseContext);
+    }
+
+    private static Cloner createCloner() {
+        Cloner cloner = new Cloner();
+        cloner.dontClone(FieldVarInitExpressionSupplier.class);
+        return cloner;
     }
 
     public static JavaContext createExecutionContext(JavaContext baseContext) {
-        JavaContext executionContext = new Cloner().deepClone(baseContext);
+        JavaContext executionContext = createCloner().deepClone(baseContext);
         executionContext.setStaticFields(baseContext.getStaticFields());
         return executionContext;
     }
--- back/src/test/java/com/server/parser/java/context/ContextCopyFactoryTest.java
+++ back/src/test/java/com/server/parser/java/context/ContextCopyFactoryTest.java
@@ -1,6 +1,6 @@
 package com.server.parser.java.context;
 
-import com.server.parser.java.ast.Variable;
+import com.server.parser.java.ast.FieldVar;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -10,19 +10,19 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class ContextCopyFactoryTest {
+    private static final String FIELD_NAME = "FIELD";
 
     @Test
     void shouldCreateExecutionContext() {
-        String key = "KEY";
-        Variable variable = mock(Variable.class);
+        FieldVar variable = mock(FieldVar.class);
         when(variable.isStatic()).thenReturn(true);
         ClassContext classContext = mock(ClassContext.class);
-        when(classContext.getFields()).thenReturn(new HashMap<String, Variable>() {{
-            put(key, variable);
+        when(classContext.getFields()).thenReturn(new HashMap<String, FieldVar>() {{
+            put(FIELD_NAME, variable);
         }});
 
         JavaContext executionContext = ContextCopyFactory.createExecutionContext(new MethodContext(classContext));
 
-        assertThat(executionContext.getStaticFields().get(key)).isSameAs(variable);
+        assertThat(executionContext.getStaticFields().get(FIELD_NAME)).isSameAs(variable);
     }
 }
