4601f76ac9f359a695225649ab1f06a4cc27cc60
DorianKossowski <doriancfc@gmail.com>
2021-02-26 22:54:59 +0100

    Create and use ContextFactory

--- back/src/main/java/com/server/parser/java/context/ContextFactory.java
+++ back/src/main/java/com/server/parser/java/context/ContextFactory.java
@@ -1,32 +1,25 @@
 package com.server.parser.java.context;
 
-import com.rits.cloning.Cloner;
-import com.server.parser.java.ast.FieldVarInitExpressionSupplier;
 import com.server.parser.java.ast.expression.Instance;
 import com.server.parser.java.call.CallResolver;
+import com.server.parser.util.ClonerFactory;
 
 public class ContextFactory {
-    private static final Class<?>[] EXCLUDED_VALIDATION_CLONING = {FieldVarInitExpressionSupplier.class};
-    private static final Class<?>[] EXCLUDED_EXECUTION_CLONING = {FieldVarInitExpressionSupplier.class, CallResolver.class};
+    private static final Class<?>[] EXCLUDED_VALIDATION_CLONING = {};
+    private static final Class<?>[] EXCLUDED_EXECUTION_CLONING = {CallResolver.class};
 
     public static JavaContext createValidationContext(JavaContext baseContext) {
-        return createCloner(EXCLUDED_VALIDATION_CLONING).deepClone(baseContext);
-    }
-
-    private static Cloner createCloner(Class<?>... cloningExcludedClasses) {
-        Cloner cloner = new Cloner();
-        cloner.dontClone(cloningExcludedClasses);
-        return cloner;
+        return ClonerFactory.createCloner(EXCLUDED_VALIDATION_CLONING).deepClone(baseContext);
     }
 
     public static JavaContext createStaticExecutionContext(JavaContext baseContext) {
-        JavaContext executionContext = createCloner(EXCLUDED_EXECUTION_CLONING).deepClone(baseContext);
+        JavaContext executionContext = ClonerFactory.createCloner(EXCLUDED_EXECUTION_CLONING).deepClone(baseContext);
         executionContext.setFields(baseContext.getStaticFields());
         return executionContext;
     }
 
     public static JavaContext createExecutionContext(Instance instance, JavaContext baseContext) {
-        JavaContext executionContext = createCloner(EXCLUDED_EXECUTION_CLONING).deepClone(baseContext);
+        JavaContext executionContext = ClonerFactory.createCloner(EXCLUDED_EXECUTION_CLONING).deepClone(baseContext);
         executionContext.setFields(instance.getFields());
         return executionContext;
     }
